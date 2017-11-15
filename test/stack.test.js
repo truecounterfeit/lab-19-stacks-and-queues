c1ade7c59e5d547c0503f9a52267d3b084ca44f4
Kelati Andemichael <Kelati@Macbooks-MacBook-Pro-2.local>
2017-11-14 19:58:32 -0800

    boilerplate

---
+++ test/stack.test.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const expect = require('expect');
+const mocha = require('mocha');
+const Stack = require('../lib/stack.js');
+
+//testing stack
+describe('Testing Stack', () => {
+it('implements last on first out funcionality', (done) => {
+    let testStack = new Stack();
+
+    testStack.push(1);
+    testStack.push(2);
+    testStack.push(3);
+
+    expect(testStack.pop()).toEqual(3);
+    expect(testStack.pop()).toEqual(2);
+    expect(testStack.pop()).toEqual(1);
+
+  });
+
+});
