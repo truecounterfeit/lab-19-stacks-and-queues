8a176b13c9b3c91f54baf64146e578cdd9cce45d
Kelati Andemichael <Kelati@Macbooks-MacBook-Pro-2.local>
2018-02-21 21:29:52 -0800

    stack test update

--- test/stack.test.js
+++ test/stack.test.js
@@ -21,19 +21,3 @@ it('implements last on first out funcionality', (done) => {
   });
 
 });
-
-describe('Testing Stack', () => {
-it('implements last on first out funcionality', (done) => {
-    let testStack = new Stack();
-
-    testStack.push(1);
-    testStack.push(2);
-    testStack.push(3);
-
-    expect(testStack.pop()).toEqual(3);
-    expect(testStack.pop()).toEqual(2);
-    expect(testStack.pop()).toEqual(1);
-
-  });
-
-});
