75a933f5b0eecf92407ecc230ffef917ab13e64b
Kelati Andemichael <Kelati@Macbooks-MacBook-Pro-2.local>
2018-02-21 22:02:29 -0800

    updated stack and queue tests

--- test/queue.test.js
+++ test/queue.test.js
@@ -2,26 +2,34 @@
 
 const expect = require('expect');
 const mocha = require('mocha');
-const queueConstructor = require('./queue- constructor');
+const queueConstructor = require('../lib/queue-constructor');
+const queue = require('../lib/queue');
 
 //testing queue
 
 describe('Testing Queue', () => {
-  let testQueue = new queueConstructor();
-
-  it('adds to end of queue', (done) => {
+  it('enqueue should insert value to end of queue', (done) => {
+    let testQueue = new queueConstructor();
 
     testQueue.enqueue(1);
     testQueue.enqueue(2);
     testQueue.enqueue(3);
 
-    expect(testQueue.enqueue()).toEqual({1, 2, 3});
+    expect(testQueue.queueConstructor.queue[0]).toEqual(1);
+    expect(testQueue.queueConstructor.queue[1]).toEqual(2);
+    expect(testQueue.queueConstructor.queue[2]).toEqual(3);
 
   });
 
-  it('subtracts from end of queue', (done) => {
+  it('pop should remove value from end of queue', (done) => {
+    let testQueue = new queueConstructor();
 
-    expect(testQueue.dequeue()).toEqual({1, 2});
+    testQueue.enqueue(1);
+    testQueue.enqueue(2);
+    testQueue.enqueue(3);
+
+    expect(testQueue.dequeue()).toEqual(1, 2);
+    
 
   });
 
--- test/stack.test.js
+++ test/stack.test.js
@@ -2,21 +2,32 @@
 
 const expect = require('expect');
 const mocha = require('mocha');
-const Stack = require('../lib/stack.js');
-const stackConstructor = require('./stack- constructor');
+const stack = require('../lib/stack');
+const stackConstructor = require('../lib/stack-constructor');
 
 //testing stack
 describe('Testing Stack', () => {
-it('implements last on first out funcionality', (done) => {
-    let testStack = new Stack();
+  it('push should insert value to end of stack', (done) => {
+    let testStack = new stackConstructor();
 
     testStack.push(1);
     testStack.push(2);
     testStack.push(3);
 
-    expect(testStack.pop()).toEqual(3);
-    expect(testStack.pop()).toEqual(2);
-    expect(testStack.pop()).toEqual(1);
+    expect(testStack.stackConstructor.stack[0]).toEqual(1);
+    expect(testStack.stackConstructor.stack[1]).toEqual(2);
+    expect(testStack.stackConstructor.stack[2]).toEqual(3);
+
+  });
+
+  it('pop should remove value from end of stack', (done) => {
+    let testStack = new stackConstructor();
+
+    testStack.push(1);
+    testStack.push(2);
+    testStack.push(3);
+
+    expect(testStack.pop()).toEqual(1, 2);
 
   });
 
